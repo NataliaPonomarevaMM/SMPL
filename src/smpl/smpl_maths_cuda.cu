ceaccd9ed51198da1f020279bd1249ab644cc7a5
NataliaPonomarevaMM <natpon12@gmail.com>
2019-10-16 02:28:15 +0300

    Fixed errors with transformations

--- src/smpl/smpl_maths_cuda.cu
+++ src/smpl/smpl_maths_cuda.cu
@@ -149,16 +149,12 @@ namespace smpl {
             int j = threadIdx.x;
 
             float elim[3];
-            for (int k = 0; k < 3; k++)
-                elim[k] = globalTransformations[i * jointnum * 16 + j * 16 + 0 * 4 + k * 4];
-
-//            float elim[3];
-//            for (int k = 0; k < 3; k++) {
-//                elim[k] = 0;
-//                for (int t = 0; t < 3; t++)
-//                    elim[k] += globalTransformations[i * jointnum * 16 + j * 16 + t * 4 + k * 4] *
-//                               joints[i * jointnum * 3 + j * 3 + t];
-//            }
+            for (int k = 0; k < 3; k++) {
+                elim[k] = 0;
+                for (int t = 0; t < 3; t++)
+                    elim[k] += globalTransformations[i * jointnum * 16 + j * 16 + t * 4 + k * 4] *
+                               joints[i * jointnum * 3 + j * 3 + t];
+            }
             for (int k = 0; k < 3; k++)
                 globalTransformations[i * jointnum * 16 + j * 16 + k * 4 + 3] = elim[k];
         }
