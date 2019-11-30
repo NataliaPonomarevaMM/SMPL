c61e92ec3d685e2f90759da977d96ce26dde35a2
NataliaPonomarevaMM <natpon12@gmail.com>
2019-11-30 20:41:06 +0300

    Updated main

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@ using clk = std::chrono::system_clock;
 
 smpl::SMPL model;
 
-void func(bool with_reinit, int num_vert) {
+void func(int num_iter, int num_vert) {
     float theta[72] = {0}, beta[10] = {0};
     float *vert = (float *)malloc(3 * num_vert * sizeof(float));
+    double *times = (double *)malloc(num_iter * sizeof(double));;
 
     for (int i = 0; i < 72; i++)
         theta[i] = rand();
@@ -21,39 +22,30 @@ void func(bool with_reinit, int num_vert) {
     for (int i = 0; i < 3 * num_vert; i++)
         vert[i] = rand();
 
-    double times[100];
-
-    const int64_t LOOPS = 100;
-    auto begin = clk::now();
-    auto end = clk::now();
-    auto duration = std::chrono::duration_cast<mcs>(end - end);
-    for (int64_t i = 0; i < LOOPS; i++) {
-        if (with_reinit) {
-            for (int i = 0; i < 72; i++)
-                theta[i] = rand();
-            for (int i = 0; i < 10; i++)
-                beta[i] = rand();
-        }
-
-        begin = clk::now();
+    if (num_vert == 0)
+        model.lbs_for_model(beta, theta);
+    else
+        model.lbs_for_custom_vertices(beta, theta, vert, num_vert);
+    double sr = 0;
+    for (int64_t i = 0; i < num_iter; i++) {
+        auto begin = clk::now();
         if (num_vert == 0)
             model.lbs_for_model(beta, theta);
         else
             model.lbs_for_custom_vertices(beta, theta, vert, num_vert);
-        end = clk::now();
+        auto end = clk::now();
         auto time = std::chrono::duration_cast<mcs>(end - begin);
-        if (i != 0)
-            duration += time;
         times[i] = (double)time.count();
-        std::cout << i << ") smpl " << times[i] << std::endl;
+        sr += times[i];
+        std::cout << i << ") smpl " << times[i] << " sr: " << sr << std::endl;
     }
-    double sr = (double)duration.count() / (LOOPS - 1.0);
+    sr /= num_iter;
     std::cout << "SR: " << sr << " mcs" << std::endl;
 
     double ds = 0;
-    for (int64_t i = 1; i < LOOPS; i++)
+    for (int64_t i = 0; i < num_iter; i++)
         ds += (times[i] - sr) * (times[i] - sr);
-    ds /= (LOOPS - 1.0);
+    ds /= num_iter;
     std::cout << "DISP: " << ds << " mcs" << std::endl;
 }
 
@@ -68,12 +60,10 @@ int main(int argc, char const *argv[]) {
     std::cout << "Time duration to load SMPL: " << (double)duration.count() / 1000000 << " s" << std::endl;
 
     std::cout << "No reinit" << std::endl;
-    func(false, 0);
-    std::cout << "With reinit" << std::endl;
-    func(true, 0);
+    func(1000, 0);
 
     std::cout << "No reinit, custom vertices" << std::endl;
-    func(false, 10);
+    func(1000, 1000);
 
     return 0;
 }
